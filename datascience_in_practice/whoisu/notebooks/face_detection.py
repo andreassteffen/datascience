035dbc6d325732a723638b15a95eb815d0d787e7
The one and only vHPC user <fisc@ip-172-31-47-133.ec2.internal>
2017-10-23 16:38:18 +0200

    Added fotos from dryrun, trained 3 models, added data augmentation for data validation.

--- datascience_in_practice/whoisu/notebooks/face_detection.py
+++ datascience_in_practice/whoisu/notebooks/face_detection.py
@@ -9,8 +9,8 @@ def convert_to_face_only(ifile, ofile, final_height=120, final_width=120, raw_re
         image = np.rot90(image,-1)
         face_locations = fr.face_locations(image)
         # we choose the detected face with the bbox that has the largest area (it's a heuristic!)
-	top,right,bottom,left = face_locations[np.argmax([np.abs(t[0]-t[2])*np.abs(t[1]-t[3]) for t in face_locations])]
-	#top,right,bottom,left = face_locations[0]
+        top,right,bottom,left = face_locations[np.argmax([np.abs(t[0]-t[2])*np.abs(t[1]-t[3]) for t in face_locations])]
+        #top,right,bottom,left = face_locations[0]
         face_image = image[top:bottom, left:right]
         small_img = Image.fromarray(face_image)
         small_img = small_img.resize((final_height, final_width))
